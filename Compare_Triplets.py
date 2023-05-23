4e7f70f310add4569352bcee72005652ef03dbf1
TejaswiniChinde <119788868+TejaswiniChinde@users.noreply.github.com>
2023-05-24 01:41:00 +0530

    a=list(map(int,input().split())) b=list(map(int,input().split())) x=y=0 for i in range(0,3):     if a[i]>b[i]:         x+=1     elif a[i]<b[i]:         y+=1 print(x,y)

---
+++ Compare_Triplets.py
@@ -0,0 +1,9 @@
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+x=y=0
+for i in range(0,3):
+    if a[i]>b[i]:
+        x+=1
+    elif a[i]<b[i]:
+        y+=1
+print(x,y)        
